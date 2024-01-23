7d2849693369fcf2448f92feec07aecdbaa995d8
Zhanton Bastiaanssen <zhantonbastiaanssen@Zhantons-MBP.freshheads.guests>
2024-01-23 15:05:44 +0100

    added search and generate function

--- my-app/page.tsx
+++ my-app/page.tsx
@@ -1,29 +1,68 @@
 import { useQuery } from "@tanstack/react-query";
-import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet, Image, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Button,
+  ActivityIndicator,
+  TextInput,
+} from "react-native";
 import { Pokemon } from "./pokemon";
+import { useState } from "react";
+
+function generateRandomPokemon() {
+  const min = 1;
+  const max = 898;
+  const random = Math.floor(Math.random() * (max - min + 1)) + min;
+  return random;
+}
 
 export default function Page() {
-  const { data: dataPokemon } = useQuery({
-    queryKey: ["pokemon", "ditto"],
-    queryFn: () =>
-      fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((res) =>
-        res.json()
-      ),
-  });
+  const [randomId, setRandomId] = useState(generateRandomPokemon());
+  const [searchInput, setSearchInput] = useState("");
 
-  const { data: dataCharacteristics } = useQuery({
-    queryKey: ["Characteristic", "ditto"],
+  const {
+    data: dataPokemon,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["pokemon", "type", randomId],
     queryFn: () =>
-      fetch("https://pokeapi.co/api/v2/characteristic/{id}/").then((res) =>
-        res.json()
-      ),
+      fetch(
+        `https://pokeapi.co/api/v2/pokemon/${searchInput || randomId}/`
+      ).then((res) => res.json()),
   });
+  
+
+  if (isLoading) {
+    return <ActivityIndicator style={styles.container} size="large" />;
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textContainer}>
+          no pokemon found, please try again
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Whitch Pokemon?"
+          keyboardType="numeric"
+          onChangeText={(text) => setSearchInput(text)}
+          value={searchInput}
+        />
+        <Button
+          title="Search again"
+          onPress={() => {
+            setRandomId(generateRandomPokemon()); // Genereer een nieuwe randomId wanneer de knop wordt ingedrukt
+          }}
+        />
+      </View>
+    );
+  }
 
   const pokemon = dataPokemon as Pokemon;
-  //   const characteristics = dataCharacteristics as Pokemon;
-  
-  console.log(dataCharacteristics);
 
   return (
     <View style={styles.container}>
@@ -33,12 +72,28 @@ export default function Page() {
       />
       <View style={styles.nameType}>
         <Text style={styles.textContainer}>{pokemon.name}</Text>
-        <Text style={styles.textContainer}>{pokemon.moves[0].move.name}</Text>
+        <Text style={styles.textContainer}>{pokemon.types[0].type.name}</Text>
       </View>
-      <View style={styles.description}>
+      <View style={styles.nameType}>
         <Text style={styles.textContainer}>{pokemon.name}</Text>
       </View>
-      <Button title="Generate"></Button>
+      <View>
+        <Text style={styles.textContainer}>{pokemon.id}</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Whitch Pokemon?"
+        onChangeText={(text) => setSearchInput(text)}
+        value={searchInput}
+      />
+      <Button
+        title="Generate"
+        onPress={() => {
+          const RandomId = generateRandomPokemon();
+          setRandomId(RandomId);
+        }}
+      />
+      
     </View>
   );
 }
@@ -61,4 +116,11 @@ const styles = StyleSheet.create({
     margin: 5,
   },
   description: {},
+  input: {
+    width: 200,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
